b2c20086ee4427d3662aa21a2a81a01b2658143e
mcicishvilii <mcicishvilii@gmail.com>
2023-08-08 02:06:37 -0700

    Initial commit

---
+++ app/src/main/java/com/example/testcomposeandersen/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.testcomposeandersen
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.testcomposeandersen.ui.screens.All
+import com.example.testcomposeandersen.ui.screens.Friends
+import com.example.testcomposeandersen.ui.screens.FromForte
+import com.example.testcomposeandersen.ui.screens.Partner
+import com.example.testcomposeandersen.ui.theme.TestComposeAndersenTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TestComposeAndersenTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TabScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TabScreen() {
+    var tabIndex by remember { mutableStateOf(0) }
+
+    val tabs = listOf("All", "Friends", "FromForte","Partner")
+
+    Column(modifier = Modifier.fillMaxWidth().padding(top = 10.dp)) {
+        TabRow(selectedTabIndex = tabIndex) {
+            tabs.forEachIndexed { index, title ->
+                Tab(text = { Text(title) },
+                    selected = tabIndex == index,
+                    onClick = { tabIndex = index },
+                )
+            }
+        }
+        when (tabIndex) {
+            0 -> All()
+            1 -> Friends()
+            2 -> FromForte()
+            3 -> Partner()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/testcomposeandersen/ui/screens/All.kt
@@ -0,0 +1,18 @@
+package com.example.testcomposeandersen.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun All() {
+    Column(modifier = Modifier.fillMaxSize().padding(top = 20.dp), horizontalAlignment = Alignment.CenterHorizontally){
+        Text(text = "All",)
+    }
+}
