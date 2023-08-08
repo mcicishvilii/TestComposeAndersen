2f92ebba248107dd9c5d3557e6b3199df7cb739a
mcicishvilii <mcicishvilii@gmail.com>
2023-08-08 10:35:52 -0700

    some changes from another branch. this seems to be working

--- app/src/main/java/com/example/testcomposeandersen/MainActivity.kt
+++ app/src/main/java/com/example/testcomposeandersen/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.testcomposeandersen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Indication
 import androidx.compose.foundation.background
@@ -15,9 +18,12 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.paddingFrom
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Divider
 import androidx.compose.material3.LinearProgressIndicator
@@ -33,16 +39,21 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.node.modifierElementOf
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.debugInspectorInfo
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.testcomposeandersen.ui.screens.All
@@ -82,6 +93,16 @@ fun TabScreen() {
     val backgroundColor = Color.hsv(0f, 0f, 0.97f)
 
 
+    val density = LocalDensity.current
+    val tabWidths = remember {
+        val tabWidthStateList = mutableStateListOf<Dp>()
+        repeat(tabs.size) {
+            tabWidthStateList.add(0.dp)
+        }
+        tabWidthStateList
+    }
+
+
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -89,17 +110,15 @@ fun TabScreen() {
 
     ) {
         TabRow(
-            selectedTabIndex = tabIndex,
             modifier = Modifier.fillMaxWidth(),
+            selectedTabIndex = tabIndex,
             divider = {},
             indicator = { tabPositions ->
-                Box(
-                    modifier = Modifier
-                        .tabIndicatorOffset(tabPositions[tabIndex])
-                        .height(4.dp)
-                        .padding(horizontal = 28.dp)
-                        .clip(RoundedCornerShape(8.dp))
-                        .background(color = indicatorColor)
+                TabRowDefaults.Indicator(
+                    modifier = Modifier.customTabIndicatorOffset(
+                        currentTabPosition = tabPositions[tabIndex],
+                        tabWidth = tabWidths[tabIndex]
+                    )
                 )
             }
         ) {
@@ -123,6 +142,10 @@ fun TabScreen() {
                                 Color.Gray
                             },
                             fontSize = 14.sp,
+                            onTextLayout = { textLayoutResult ->
+                                tabWidths[tabIndex] =
+                                    with(density) { textLayoutResult.size.width.toDp() }
+                            }
 
                             )
                         if (title.tabNotiCount > 0) {
@@ -177,3 +200,26 @@ data class Tabs(
     val tabName:String,
     val tabNotiCount:Int
 )
+
+fun Modifier.customTabIndicatorOffset(
+    currentTabPosition: TabPosition,
+    tabWidth: Dp
+): Modifier = composed(
+    inspectorInfo = debugInspectorInfo {
+        name = "customTabIndicatorOffset"
+        value = currentTabPosition
+    }
+) {
+    val currentTabWidth by animateDpAsState(
+        targetValue = tabWidth,
+        animationSpec = tween(durationMillis = 250, easing = FastOutSlowInEasing)
+    )
+    val indicatorOffset by animateDpAsState(
+        targetValue = ((currentTabPosition.left + currentTabPosition.right - tabWidth) / 2),
+        animationSpec = tween(durationMillis = 250, easing = FastOutSlowInEasing)
+    )
+    fillMaxWidth()
+        .wrapContentSize(Alignment.BottomStart)
+        .offset(x = indicatorOffset)
+        .width(currentTabWidth)
+}
